0938f2246ba663ad32beb52108025dc5e47e56cc
Cliff Rodgers <kliph@users.noreply.github.com>
2017-04-20 19:50:57 -0400

    Remove docs and render registration form

--- src/cljs/picture_gallery/components/common.cljs
+++ src/cljs/picture_gallery/components/common.cljs
@@ -1,5 +1,30 @@
 (ns picture-gallery.components.common)
 
+(defn input [type id placeholder fields]
+  [:input.form-control.input-lg
+   {:type type
+    :placeholder placeholder
+    :value (id @fields)
+    :on-change #(swap! fields assoc id (-> %
+                                           .-target
+                                           .-value))}])
+
+(defn form-input [type label id placeholder fields optional?]
+  [:div.form-group
+   [:label label]
+   (if optional?
+     [input type id placeholder fields]
+     [:div.input-group
+      [input type id placeholder fields]
+      [:span.input-group-addon
+       "*"]])])
+
+(defn text-input [label id placeholder fields & [optional?]]
+  (form-input :text label id placeholder fields optional?))
+
+(defn password-input [label id placehollder fields & [optional?]]
+  (form-input :password label id placeholder fields optional?))
+
 (defn modal [header body footer]
   [:div
    [:div.modal-dialog
