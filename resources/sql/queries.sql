0a650e193df9d0ca8f22222c0bc36c9d04ff8b14
Cliff Rodgers <kliph@users.noreply.github.com>
2017-04-20 17:10:39 -0400

    Need to test adding and retrieving users
    See: https://forums.pragprog.com/forums/387/topics/14104

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,14 +1,8 @@
 -- :name create-user! :! :n
 -- :doc creates a new user record
 INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
-
--- :name update-user! :! :n
--- :doc update an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
+(id, pass)
+VALUES (:id, :pass)
 
 -- :name get-user :? :1
 -- :doc retrieve a user given the id.
